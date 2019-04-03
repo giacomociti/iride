46ec5586cd2f883323f2e93fdddeef3d94552c2f
giacomociti <giacomociti@gmail.com>
2019-04-03 23:42:26 +0200

    Improve tests

--- tests/Iride.Tests/Iride.Sparql.Tests.fs
+++ tests/Iride.Tests/Iride.Sparql.Tests.fs
@@ -1,35 +1,55 @@
 module IrideSparqlTests
 
 open NUnit.Framework
-open Iride
-open VDS.RDF.Storage
 open VDS.RDF
+open VDS.RDF.Storage
+open Iride
+open System
+
+let nodeFactory = NodeFactory()
+let literal = nodeFactory.CreateLiteralNode
+
+let storage =
+    let inMemoryManager = new InMemoryManager()
+    inMemoryManager.Update """INSERT DATA {
+        <http://example.org/s> <http://example.org/p> "aa"
+    }"""
+    inMemoryManager :> IQueryableStorage
+
 
 type Ask = SparqlCommand<"ASK WHERE {?s ?p $o}">
 
 [<Test>]
 let ``Can ask`` () =
-    let storage = new InMemoryManager()
     let cmd = Ask(storage)
-    let aa = NodeFactory().CreateLiteralNode("aa")
-    let bb = NodeFactory().CreateLiteralNode("bb")
-    //Assert.True(cmd.Run(aa))
-    Assert.False(cmd.Run(bb))
+    cmd.Run(literal "bb") |> Assert.False
+    cmd.Run(literal "aa") |> Assert.True
 
 type Construct = SparqlCommand<"CONSTRUCT {?s ?p $o} WHERE {?s ?p $o}">
 
 [<Test>]
 let ``Can constuct`` () =
-    let storage = new InMemoryManager()
     let cmd = Construct(storage)
-    let aa = NodeFactory().CreateLiteralNode("aa")
-    Assert.True(cmd.Run(aa).IsEmpty)
+    cmd.Run(literal "bb").IsEmpty |> Assert.True
+    
+    let graph = cmd.Run(literal "aa")
+    graph.IsEmpty |> Assert.False
+    let triple = graph.Triples |> Seq.exactlyOne
+    let s = (triple.Subject :?> IUriNode).Uri
+    let p = (triple.Predicate :?> IUriNode).Uri
+    let o = (triple.Object :?> ILiteralNode).Value
+    Assert.AreEqual(Uri "http://example.org/s", s)
+    Assert.AreEqual(Uri "http://example.org/p", p)
+    Assert.AreEqual("aa", o)
+
 
 type Select = SparqlCommand<"SELECT * WHERE {?s ?p $o}">
 
 [<Test>]
 let ``Can select`` () =
-    let storage = new InMemoryManager()
     let cmd = Select(storage)
-    let aa = NodeFactory().CreateLiteralNode("aa")
-    Assert.True(cmd.Run(aa).Length=0)
+    let result = cmd.Run(literal "aa") |> Seq.exactlyOne
+    let s = (result.s :?> IUriNode).Uri
+    let p = (result.p :?> IUriNode).Uri
+    Assert.AreEqual(Uri "http://example.org/s", s)
+    Assert.AreEqual(Uri "http://example.org/p", p)
