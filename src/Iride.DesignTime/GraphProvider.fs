22279714972b456316d2dcdd3f19400ae58dfb40
Giacomo Citi <giacomociti@gmail.com>
2020-11-09 09:13:50 +0100

    little cleanup

--- src/Iride.DesignTime/GraphProvider.fs
+++ src/Iride.DesignTime/GraphProvider.fs
@@ -1,16 +1,14 @@
 ﻿module GraphProviderProviderImplementation
 
+open System
 open System.Reflection
 open Microsoft.FSharp.Quotations
 open FSharp.Core.CompilerServices
 open ProviderImplementation.ProvidedTypes
 open Iride
 open Iride.SparqlHelper
-open VDS.RDF.Query
-open VDS.RDF.Parsing
 open TypeProviderHelper
 open VDS.RDF
-open System
 
 [<TypeProvider>]
 type GraphProvider (config : TypeProviderConfig) as this =
@@ -25,65 +23,57 @@ type GraphProvider (config : TypeProviderConfig) as this =
     do assert (typeof<CommandRuntime>.Assembly.GetName().Name = executingAssembly.GetName().Name)
 
     let addCtorWithField (providedType: ProvidedTypeDefinition, fieldName, fieldType) =
+        let parameter = ProvidedParameter(fieldName, fieldType)
         let field = ProvidedField("_" + fieldName, fieldType)
+        // TODO set as private readonly
         providedType.AddMember field
-
         //TODO add public getter?
-
-        let ctor = 
-            ProvidedConstructor(
-                [ProvidedParameter(fieldName, fieldType)], 
-                invokeCode = fun args -> 
-                    match args with
-                    | [this; result] ->
-                      Expr.FieldSet (this, field, result)
-                    | _ -> failwith "wrong ctor params")
-        providedType.AddMember ctor
-        field
+        ProvidedConstructor([parameter], invokeCode = function
+            | [this; arg] ->
+                Expr.FieldSet (this, field, arg)
+            | _ -> failwith "wrong ctor params")
+        |> providedType.AddMember 
+        
 
     let literalType = function
-    | "http://www.w3.org/2001/XMLSchema#string" -> KnownDataType.Literal
-    | "http://www.w3.org/2001/XMLSchema#integer" -> KnownDataType.Integer
-    | "http://www.w3.org/2001/XMLSchema#date" -> KnownDataType.Date
-    | "http://www.w3.org/2001/XMLSchema#dateTime" -> KnownDataType.Time
-    | "http://www.w3.org/2001/XMLSchema#decimal" -> KnownDataType.Number
-    | "http://www.w3.org/2001/XMLSchema#boolean" -> KnownDataType.Boolean
-    | _ -> KnownDataType.Node
+        | "http://www.w3.org/2001/XMLSchema#string" -> KnownDataType.Literal
+        | "http://www.w3.org/2001/XMLSchema#integer" -> KnownDataType.Integer
+        | "http://www.w3.org/2001/XMLSchema#date" -> KnownDataType.Date
+        | "http://www.w3.org/2001/XMLSchema#dateTime" -> KnownDataType.Time
+        | "http://www.w3.org/2001/XMLSchema#decimal" -> KnownDataType.Number
+        | "http://www.w3.org/2001/XMLSchema#boolean" -> KnownDataType.Boolean
+        | _ -> KnownDataType.Node
 
     let getValuesMethodInfo elementType =
         let methodInfo = TypeProviderHelper.getMethodInfo <@@ CommandRuntime.GetValues(Unchecked.defaultof<INode>, "", id) @@>
         ProvidedTypeBuilder.MakeGenericMethod(methodInfo.GetGenericMethodDefinition(), [elementType])
 
+    let createTypeWithField (providedAssembly, typeName, fieldName, fieldType) =
+        let result = ProvidedTypeDefinition(providedAssembly, ns, typeName, Some typeof<obj>, isErased=false)
+        addCtorWithField (result, fieldName, fieldType)
+        result
+
     let createType (typeName, sample) =
         let providedAssembly = ProvidedAssembly()
-        let providedType = ProvidedTypeDefinition(providedAssembly, ns, typeName, Some typeof<obj>, isErased=false)
+        let providedType = createTypeWithField(providedAssembly, typeName, "graph", typeof<IGraph>)
 
-        let _graphField = addCtorWithField(providedType, "graph", typeof<IGraph>)
-
-        let classes = 
+        let types = 
             sample
             |> RdfHelper.getGraph config.ResolutionFolder 
             |> GraphHelper.sample2classes
-            |> Seq.toArray
-
-        let types =
-            classes
-            |> Array.map (fun x -> x.Name, ProvidedTypeDefinition(providedAssembly, ns, RdfHelper.getName x.Name, Some typeof<obj>, isErased=false))
+            |> Seq.map (fun x -> x.Name, (x, createTypeWithField(providedAssembly, RdfHelper.getName x.Name, "node", typeof<INode>)))
             |> dict
 
-        for c in classes do
-            let t = types.[c.Name]
-            ignore <| addCtorWithField(t, "node", typeof<INode>)
+        for entry in types do
+            let classDefinition = fst entry.Value
+            let typeDefinition = snd entry.Value
+            let nodeField = typeDefinition.GetField("_node", BindingFlags.NonPublic ||| BindingFlags.Instance)
 
-        for c in classes do
-            let t = types.[c.Name]
-            let nodeField = t.GetField("_node", BindingFlags.NonPublic ||| BindingFlags.Instance)
-
-            c.Properties
+            classDefinition.Properties
             |> Seq.map (fun p ->
                 match p.Value with
                 | GraphHelper.PropertyType.Class x -> 
-                    let elementType = types.[x] :> Type
+                    let elementType = snd types.[x] :> Type
                     let resultType = ProvidedTypeBuilder.MakeGenericType(typedefof<seq<_>>, [elementType])
                     let getValuesMethod = getValuesMethodInfo elementType
                     let predicateUri = Expr.Value p.Key.AbsoluteUri
@@ -109,18 +99,10 @@ type GraphProvider (config : TypeProviderConfig) as this =
                         let subject = Expr.FieldGet(this, nodeField)
                         Expr.Call(getValuesMethod, [subject; predicateUri; objectConverter])
                     | _ -> failwith "Expected a single parameter"))
-                    
             |> Seq.toList
-            |> t.AddMembers
+            |> typeDefinition.AddMembers
 
-        //for c in classes do 
-            ProvidedProperty("Get" +  RdfHelper.getName c.Name, types.[c.Name].MakeArrayType(), getterCode = function
-            | [this] ->
-                <@@ Array.empty @@>
-            | _ -> failwith "Expected a single parameter")
-            |> providedType.AddMember
-
-        for t in types do providedType.AddMember t.Value
+        for t in types do providedType.AddMember (snd t.Value)
 
         providedAssembly.AddTypes [providedType]
         providedType
